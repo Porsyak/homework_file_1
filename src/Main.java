a19972acc83a22ba8821dedf0f2d76c6b9d7ecf5
Ivan Porsyak <2663634@gmail.com>
2022-03-30 18:36:33 +0300

    Start refactoring code -add metod addDirGame.

--- src/Main.java
+++ src/Main.java
@@ -1,126 +1,30 @@
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
-        StringBuilder sb = new StringBuilder();
+    private static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        String dirGames = "/Users/ivanporsak/IdeaProjects/Game";
+        List<String> listDir = Arrays.asList("src", "res", "savegames", "temp");
+        addDirGame(dirGames,listDir);
+    }
 
-
-        File fileSrc = new File("/Users/ivanporsak/IdeaProjects/Game/src");
-        if (fileSrc.mkdir()){
-            sb.append("Директория src создана");
-        }
-        else {
-            sb.append("\n ОШИБКА! Директория src не создана или уже существует");
-        }
-        File fileSrcMain = new File("/Users/ivanporsak/IdeaProjects/Game/src/main");
-        if (fileSrcMain.mkdir()){
-            sb.append("\n Директория src/main создана");
-        }
-        else {
-            sb.append("\n ОШИБКА ! Директория src/main не создана или уже существует");
-        }
-        File fileSrcMaimFile = new File("/Users/ivanporsak/IdeaProjects/Game/src/main/main.java");
-        try{
-            if(fileSrcMaimFile.createNewFile()){
-            sb.append("\n Файл main.java создан");
-            }
-        }catch (IOException ioe){
-            sb
-                    .append("\n")
-                    .append(ioe.getMessage());
-        }
-        File fileSrcMaimUtils = new File("/Users/ivanporsak/IdeaProjects/Game/src/main/utils.java");
+    public static void addDirGame(String dir, List<String> list){
         try {
-            if(fileSrcMaimUtils.createNewFile()){
-                sb.append("\n Файл utils.java создан");
-            }
-        }catch (IOException ioException){
+        for (String s : list) {
+            Files.createDirectories(Path.of(dir + "/" + s));
             sb
-                    .append("\n")
-                    .append(ioException.getMessage());
+                    .append("Создана директория: ")
+                    .append(dir)
+                    .append(s);
         }
-
-
-        File fileSrcTest = new File("/Users/ivanporsak/IdeaProjects/Game/src/test");
-        if (fileSrcTest.mkdir()){
-            sb.append("\n Директория src/test создана");
         }
-        else {
-            sb.append("\n ОШИБКА ! Директория src/test не создана или уже существует");
-        }
-
-
-        File fileRes = new File("/Users/ivanporsak/IdeaProjects/Game/res");
-        if (fileRes.mkdir()){
-            sb.append("\n Директория res созданна");
-        }
-        else {
-            sb.append("\n ОШИБКА! Директория res не создана или уже существует");
-        }
-
-
-        File fileResDrawables = new File("/Users/ivanporsak/IdeaProjects/Game/res/drawables");
-        if (fileResDrawables.mkdir()){
-            sb.append("\n Директория res/drawables создана");
-        }
-        else {
-            sb.append("\n ОШИБКА ! Директория не создана или уже существует");
-        }
-
-
-        File fileResVectors = new File("/Users/ivanporsak/IdeaProjects/Game/res/vectors");
-        if(fileResVectors.mkdir()){
-            sb.append("\n Директория res/vectors создана");
-        }
-        else {
-            sb.append("\n ОШИБКА ! Директория res/vectors не создана или уже сужествует");
-        }
-
-
-        File fileResIcons = new File("/Users/ivanporsak/IdeaProjects/Game/res/icons");
-        if(fileResIcons.mkdir()){
-            sb.append("\n Директория res/icons создана");
-        }
-        else {
-            sb.append("\n ОШИБКА ! Директория res/icons не создана или уже существует ");
-        }
-
-
-        File fileSavegames = new File("/Users/ivanporsak/IdeaProjects/Game/savegames");
-        if (fileSavegames.mkdir()){
-            sb.append("\n Директория savegames создана");
-        }
-        else {
-            sb.append("\n ОШИБКА ! Директория savegsmes не создана или уже существует");
-        }
-
-
-        File fileTemp = new File("/Users/ivanporsak/IdeaProjects/Game/temp");
-        if (fileTemp.mkdir()){
-            sb.append("\n Директория temp создана");
-        }
-        else {
-            sb.append("\n ОШИБКА! Директория temp не создана или уже существует");
-        }
-        File fileTempFile = new File("/Users/ivanporsak/IdeaProjects/Game/temp/temp.txt");
-        try {
-            if(fileTempFile.createNewFile()){
-                sb.append("\n Файл temp.txt создан");
-            }
-        }catch (IOException e){
-            sb
-                    .append("\n")
-                    .append(e.getMessage());
-        }
-
-
-        try{
-            FileWriter fileWriter = new FileWriter(fileTempFile);
-            String str = sb.toString();
-            fileWriter.write(str);
-            fileWriter.close();
-        }catch (IOException e){
-            System.out.println(e.getMessage());
+        catch (IOException ioException){
+            sb.append(ioException.getMessage());
         }
     }
 }
