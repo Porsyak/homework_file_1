84b5120e77d477fb15b476a2e4a98f9acc85c8bd
Ivan Porsyak <2663634@gmail.com>
2022-03-30 22:47:19 +0300

    Add two metods end two ArrayList -listDirSrc -listDirRes

--- src/Main.java
+++ src/Main.java
@@ -7,10 +7,21 @@ import java.util.List;
 
 public class Main {
     private static StringBuilder sb = new StringBuilder();
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args){
+
         String dirGames = "/Users/ivanporsak/IdeaProjects/Game";
+        String dirSrc = "/src";
+        String dirRes = "/res";
+
         List<String> listDir = Arrays.asList("src", "res", "savegames", "temp");
+        List<String> listDirSrc = Arrays.asList("main", "test");
+        List<String> listDirRes = Arrays.asList("drawables", "vectors", "icons");
+
         addDirGame(dirGames,listDir);
+        addDirGame(dirGames + dirSrc, listDirSrc);
+        addDirGame(dirGames + dirRes, listDirRes);
+
+
     }
 
     public static void addDirGame(String dir, List<String> list){
@@ -20,7 +31,8 @@ public class Main {
             sb
                     .append("Создана директория: ")
                     .append(dir)
-                    .append(s);
+                    .append(s)
+                    .append("\n");
         }
         }
         catch (IOException ioException){
