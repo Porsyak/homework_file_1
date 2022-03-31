c4e75bd913b496a5374e12c789f563a95cfcdd3b
Ivan Porsyak <2663634@gmail.com>
2022-03-31 13:03:30 +0300

    Add all metods Need to record temp.txt

--- src/Main.java
+++ src/Main.java
@@ -7,21 +7,36 @@ import java.util.List;
 
 public class Main {
     private static StringBuilder sb = new StringBuilder();
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         String dirGames = "/Users/ivanporsak/IdeaProjects/Game";
-        String dirSrc = "/src";
-        String dirRes = "/res";
 
         List<String> listDir = Arrays.asList("src", "res", "savegames", "temp");
         List<String> listDirSrc = Arrays.asList("main", "test");
         List<String> listDirRes = Arrays.asList("drawables", "vectors", "icons");
 
         addDirGame(dirGames,listDir);
-        addDirGame(dirGames + dirSrc, listDirSrc);
-        addDirGame(dirGames + dirRes, listDirRes);
+        addDirGame(dirGames + "/src", listDirSrc);
+        addDirGame(dirGames + "/res", listDirRes);
 
+        addFiles(dirGames + "/src/main/Main.java");
+        addFiles(dirGames + "/src/main/Utils.java");
+        addFiles(dirGames + "/temp/temp.txt");
+        String log = sb.toString();
+        //Files.write(Path.of("/Users/ivanporsak/IdeaProjects/Game/temp/temp.txt"));
 
+    }
+
+    public static void addFiles(String nameFile){
+        try {Files.createFile(Path.of(nameFile));
+            sb
+                    .append("Файл успешно создан: ")
+                    .append(nameFile)
+                    .append("\n");
+        }
+        catch (IOException e){
+             sb.append(e.getMessage());
+        }
     }
 
     public static void addDirGame(String dir, List<String> list){
--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,13 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Test {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        //Path path = Paths.get("/Users/ivanporsak/IdeaProjects/Game/src/main/path.txt");
+       // Files.createFile(Path.of("/Users/ivanporsak/IdeaProjects/Game/src/main/path.txt"));
+
 
 
     }
